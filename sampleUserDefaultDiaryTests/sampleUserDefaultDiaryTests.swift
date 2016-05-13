dda8cef668d5d9d745ed9af584ed147fc18f32b7
Riho Okubo <rihoohkubo1002@gmail.com>
2016-05-13 13:44:00 +0800

    Initial Commit

---
+++ sampleUserDefaultDiaryTests/sampleUserDefaultDiaryTests.swift
@@ -0,0 +1,36 @@
+//
+//  sampleUserDefaultDiaryTests.swift
+//  sampleUserDefaultDiaryTests
+//
+//  Created by RIho OKubo on 2016/05/13.
+//  Copyright © 2016年 RIho OKubo. All rights reserved.
+//
+
+import XCTest
+@testable import sampleUserDefaultDiary
+
+class sampleUserDefaultDiaryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
